1073ebbdb61208b20c962bcf44754dfba08a3284
nashbe26 <ala.touati@outlook.fr>
2021-03-18 19:46:49 +0100

    first push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',component:HomeComponent},
+  { path:'accueil',component:AccueilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostChildComponent } from './components/post-child/post-child.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    PostChildComponent,
+    PostsComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
